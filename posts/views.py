55c6295dfe15bc8c7334ab33c3063d3961b1b667
yukiFunasaka <ffffna0lucky@gmail.com>
2021-01-10 14:26:18 +0900

    最初のコミット

---
+++ posts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+def index(request):
+    # return HttpResponse("Hello world! このページは投稿のインデックスです。")
+    posts = Post.objects.order_by('-published')
+    return render(request, 'posts/index.html', {'posts' : posts})
+
+def post_detail(request,post_id):
+    return render(request, 'posts/post_detail.html',{'post_id':post_id})
+# Create your views here.
